171f911ae822c27999d925fa19710f919fb1e86d
Rohit Kumar 12 <rku165@sapient.com>
2016-11-18 21:05:00 +0530

    Adding D3 and React

---
+++ D3-work/d3-src.js
@@ -0,0 +1,27 @@
+(function() {
+
+    function draw(iData) {
+        var list = d3.select('body div').selectAll('section');
+        list = list.data(iData);
+        list = list.enter();
+        list = list.append('section');
+        list = list.text(function(element) {
+            return element.name + ' - ' + element.status;
+        }).style('color', function(d) {
+            if (d.status == 'online' || d.status == 'available')
+                return 'lime';
+            return 'red';
+        });
+    }
+
+    var data = [{ name: 'Rohit Kumar', status: 'offline' }, { name: 'Rahul Kumar', status: 'online' },
+        { name: 'Munuu', status: 'offline' },
+        { name: 'Ritu kumari', status: 'away' },
+        { name: 'Arman', status: 'available' },
+        { name: 'Ranveer', status: 'unknown' }
+    ];
+
+
+    draw(data);
+
+})();
